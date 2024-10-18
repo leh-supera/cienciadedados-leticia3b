3557363ae9340a297f92b8315a1ed563d570d0c3
leh-supera <leticia.stefany.silva@escola.pr.gov.br>
2024-10-18 07:53:45 -0300

    leticia276

--- graficos/informacoes globais.js
+++ graficos/informacoes globais.js
@@ -1,5 +1,7 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
+console.log(url)
+
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
